bf4736d30c67e2d11446d2778df3d3821415c554
j88moja-code <sitholent+github@gmail.com>
2023-04-18 18:46:09 +0000

    Make correction

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python3
-""" Pagination """
-
+"""Implement a method named get_page that takes two integer
+arguments page with default value 1 and page_size with default value 10
+"""
 import csv
 import math
 from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """
+    Parameters
+    ----------
+    page : int
+        page number
+    page_size : int
+        size of page
+    Returns
+    -------
+    tuple
+        a tuple of size two containing a start
+        index and an end index corresponding to
+        the range of indexes to return in a list
+        for those particular pagination parameters.
+    """
+    if page <= 1:
+        return (0, page_size)
+    else:
+        return ((page * page_size) - page_size, (page * page_size))
 
 
 class Server:
@@ -26,8 +49,16 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        pass
+        """Get page from csv dataset"""
+        try:
+            assert page > 0
+            assert page_size > 0
+        except Exception as e:
+            raise AssertionError
 
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """ returns tuple of size two containing start index, end index """
-        pass
+        dataset = self.dataset()
+
+        if page > len(dataset) or page_size > len(dataset):
+            return []
+        indexes = index_range(page, page_size)
+        return dataset[indexes[0]:indexes[1]]
